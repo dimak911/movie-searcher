850d9f5916db7689a05761b5a343afc2e7c4fee0
dmytrokniahnytskyi <dimak911@gmail.com>
2023-01-19 19:43:19 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { App } from "components/App";
-import { ThemeProvider } from "styled-components";
-import { theme } from "./theme";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from 'components/App';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './theme';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/movie-searcher/">
+    <BrowserRouter basename="/">
       <ThemeProvider theme={theme}>
         <App />
       </ThemeProvider>
